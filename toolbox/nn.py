1b19cf98d33596efff23354ba91a60c802814193
WonJae <w990417@gmail.com>
2023-12-08 02:06:37 +0900

    second_run

--- toolbox/nn.py
+++ toolbox/nn.py
@@ -38,20 +38,18 @@ class SoftmaxCNN(nn.Module):
         self.batchnorm2 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(64 * 21 * 42, 128)
-        self.batchnorm3 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 52)
         self.softmax = nn.Softmax(dim=2) # (batch_size, 4, 13)
 
 
     def forward(self, x):
-        x = self.batchnorm1(self.conv1(x))
-        x = self.pool(torch.relu(x))
-        x = self.batchnorm2(self.conv2(x))
-        x = self.pool(torch.relu(x))
-        x = x.view(-1, 64 * 21 * 42)
+        x = torch.relu(self.batchnorm1(self.conv1(x)))
+        x = self.pool(x)
+        x = torch.relu(self.batchnorm2(self.conv2(x)))
+        x = self.pool(x)
         
-        x = self.batchnorm3(self.fc1(x))
-        x = torch.relu(x)
+        x = x.view(-1, 64 * 21 * 42)
+        x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         x = x.view(-1, 4, 13) # reshape to match chord_frame's shape
         x = self.softmax(x)
